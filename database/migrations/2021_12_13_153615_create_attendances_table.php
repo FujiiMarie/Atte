a1bff9c4c9576f8bf577cf04ea8f9b2371aadfe2
FujiiMarie <fujimari45@gmail.com>
2021-12-13 16:38:38 +0900

    make-er-migration

---
+++ database/migrations/2021_12_13_153615_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->integer('worker_id');
+            $table->date('work_day');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->time('break_start');
+            $table->time('break-end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
