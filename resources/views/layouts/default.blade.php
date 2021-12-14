380b6bc2556b7b263259becb4222c0117f4581ac
FujiiMarie <fujimari45@gmail.com>
2021-12-15 02:18:09 +0900

    make-controller-blade

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,28 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+
+  <title>@yield('title')</title>
+  <style>
+    body {
+      font-size: 16px;
+      margin: 5px;
+    }
+    h1 {
+      font-size: 30px;
+      margin: 10px;
+    }
+    .content {
+      margin: 10px;
+    }
+  </style>
+
 </head>
+
 <body>
-  
+  <h1>@yield('title')</h1>
+  <div class="content">
+    @yield('content')
+  </div>
 </body>
 </html>
---
+++ resources/views/wokers/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('title')
+<h1>Atte</h1>
+@endsection
