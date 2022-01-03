7361ff9e563072fe6e94168a8027fb1ec655558f
FujiiMarie <fujimari45@gmail.com>
2022-01-04 02:26:19 +0900

    ホーム画面ヘッダー部分の修正-1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Atte') }}
+            {{ __('ここを無くしたい') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="flex justify-center text-xl font-bold py-12">{{ Auth::user()->name }}さんお疲れ様です！</div>
+
+    <div class="pb-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="text-xl font-bold text-center space-y-10">
                 <div class="flex flex-wrap justify-center space-x-10"> 
