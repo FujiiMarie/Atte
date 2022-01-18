06640d9212de12fbf3d5f1bb491fc6ecf2700d80
Marie Fujii <fujimari45@gmail.com>
2022-01-18 20:13:01 +0900

    勤怠登録画面の初期処理4

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -15,52 +15,55 @@ class AttendanceController extends Controller
     //ホーム
     public function index(Request $request)//ログイン時にどのボタンが押せるかの画面表示
     {
-        $btn_display->btn_start_time = false;//trueならボタン表示
-        $btn_display->btn_end_time = false;
-        $btn_display->btn_start_rest = false;
-        $btn_display->btn_end_rest = false;
+        $btn_display = [//trueならボタン表示
+            'btn_start_time' => false,
+            'btn_end_time' => false,
+            'btn_start_rest' => false,
+            'btn_end_rest' => false,
+        ];
+
+        $btn_start_time = $btn_display['btn_start_time'];
+        $btn_end_time = $btn_display['btn_end_time'];
+        $btn_start_rest = $btn_display['btn_start_rest'];
+        $btn_end_rest = $btn_display['btn_end_rest'];
 
         $user = Auth::user();
-        $user_id = $user->id;
+        $user_id = Auth::id();
         $today = Carbon::today()->format('Y-m-d');
         $attendance = Attendance::where('user_id', $user_id)->where('work_day', $today)->first();
         // $attendanceの中身{id,user_id,work_day,start_time,end_time,create_date,update_date}
 
-        Log::alert('useridの出力調査', ['$attendance_user_id' => $attendance->user_id]);
-        Log::alert('useridの出力調査', ['$attendance_work_day' => $attendance->work_day]);
-        Log::alert('useridの出力調査', ['$attendance_end_time' => $attendance->end_time]);
+        //デバッグ
+        Log::alert('useridの出力調査', ['Auth_user_id' => $user_id]);
+        Log::alert('useridの出力調査', ['today' => $today]);
+        // Log::alert('useridの出力調査', ['$attendance_user_id' => $attendance->user_id]);
+        // Log::alert('useridの出力調査', ['$attendance_work_day' => $attendance->work_day]);
+        // Log::alert('useridの出力調査', ['$attendance_end_time' => $attendance->end_time]);
 
         //①今日出勤しているかどうか？
         if($attendance != null){ //データがある場合:「勤務開始」ボタンが押せない
-            $btn_display->btn_start_time = false;
+            return view('attendanceregister', ['btn_start_time' => $btn_start_time]);
 
             //②勤務終了時間が入っているか？
-            if($attendance->btn_end_time != null){//入っている場合：全てのボタンが押せない
-                $btn_display = false;
+            if($attendance->end_time != null){//入っている場合：全てのボタンが押せない
+                return view('attendanceregister', $btn_display);
             }else{//入っていない場合：③休憩中かどうか？
                 $rest = Rest::where('user_id', $user_id)->where('work_day', $today)->first();
-
                 // start_restをorder byで指定かつ一番初めのデータを持ってくる
                 if($rest->start_rest != null){//データがある場合：④休憩終了時間があるかどうか？
 
                     if($rest->end_rest != null){//入っている場合（休憩終了）：「勤務終了」「休憩開始」ボタンが押せる
-                        $btn_display->btn_end_time = true;
-                        $btn_display->btn_rest_start = true;
-                    }else{
-                        $btn_display->btn_rest_end = true;//入っていない場合（休憩中）：「休憩終了」ボタンが押せる    
+                        return view('attendanceregister', ['btn_end_time' => $btn_end_time], ['btn_rest_start' => $btn_rest_start]);//←trueにしたい
+                    }else{//入っていない場合（休憩中）：「休憩終了」ボタンが押せる
+                        return view('attendanceregister', ['btn_rest_end' => $btn_rest_end]);//←trueにしたい
                     }
                 }else{//データがない場合：（休憩していない）：「勤務終了」「休憩開始」ボタンが押せる
-                    $btn_display->btn_end_time = true;
-                    $btn_display->btn_start_rest = true;
+                    return view('attendanceregister', ['btn_end_time' => $btn_end_time], ['btn_rest_start' => $btn_rest_start]);//←trueにしたい
                 }            
             }
         }else{//データがない場合:「勤務開始」ボタンが押せる
-            $btn_display->btn_start_time = true;
+            return view('attendanceregister', ['btn_start_time' => $btn_start_time]);//←trueにしたい
         }
-        //bladeに$btn_displayを渡す
-        
-
-        return view('attendanceregister',compact('btn_display'));
     }
 
 
